7455538e0fc83bd8169a477e896ed4f87dcc5f3b
unknown <waylonbr@HPMXL2182CH0.cs.tamu.edu>
2015-02-26 12:50:08 -0600

    Hands dont knock over guns

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -36,13 +36,36 @@ public class Hand : MonoBehaviour
         pickupframe = false;
     }
 
+	private void OnTriggerStay(Collider col)
+	{
+		if (((rightHand && Input.GetKeyDown(KeyCode.E)) || (!rightHand && Input.GetKeyDown(KeyCode.Q))) && !carrying)
+		{
+			//print ("okay");
+			//Pickupable p = col.collider.gameObject as Pickupable;
+			Pickupable p = col.collider.gameObject.GetComponent<Pickupable>();
+			if (p != null)
+			{
+				carrying = true;
+				carriedObject = p;
+				carriedObject.rigidbody.isKinematic = true;
+				carriedObject.transform.parent = this.gameObject.transform;
+				carriedObject.transform.localPosition = Vector3.zero;
+				carriedObject.transform.localRotation = Quaternion.identity;
+				
+				pickupframe = true;
+				
+				CrossHair.drawCrosshair = false;
+			}
+		}
+	}
+
 
     void OnCollisionEnter(Collision col)
     {
 		//print ("pizza");
     }
 
-    void OnCollisionStay(Collision col)
+    /*void OnCollisionStay(Collision col)
     {
         if (((rightHand && Input.GetKeyDown(KeyCode.E)) || (!rightHand && Input.GetKeyDown(KeyCode.Q))) && !carrying)
         {
@@ -63,5 +86,5 @@ public class Hand : MonoBehaviour
                 CrossHair.drawCrosshair = false;
             }
         }
-    }
+    }*/
 }
