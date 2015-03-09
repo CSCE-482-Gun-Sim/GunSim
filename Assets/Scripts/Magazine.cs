c54aa0e223fbc4676046731ca0e5ac7c801e40c1
Waylon Brown <waylonjbrown@gmail.com>
2015-03-08 23:23:02 -0500

    Objects now slowly move before snapping

--- Assets/Scripts/Magazine.cs
+++ Assets/Scripts/Magazine.cs
@@ -8,18 +8,25 @@ public class Magazine : MonoBehaviour {
 		Hand,
 		Gun
 	};
+	GameObject hand;
+	GameObject pistol;
+
 	public Vector3 handRotation;
 	public Vector3 pistolRotation;
 	public Vector3 handPosition;
 	public Vector3 pistolPosition;
-	bool needsToLerp = false;
-	float timer;
+	public Vector3 lerpHandPosition;
+	public Vector3 lerpPistolPosition;
 
+	bool needsToLerp = false;
+	float speed = 5.0f;
 	AttachPoint attached;
 	
 	// Use this for initialization
 	void Start () {
 		attached = AttachPoint.None;	
+		hand = GameObject.FindGameObjectWithTag ("LeftHand");
+		pistol = GameObject.FindGameObjectWithTag ("Pistol");
 	}
 
 	// Update is called once per frame
@@ -30,6 +37,7 @@ public class Magazine : MonoBehaviour {
 			if (attached == AttachPoint.Hand){
 				needsToLerp = true;
 				attached = AttachPoint.Gun;
+				this.rigidbody.isKinematic = true;
 			} else if (attached == AttachPoint.Gun){
 				attached = AttachPoint.None;
 				this.rigidbody.isKinematic = false;
@@ -37,61 +45,74 @@ public class Magazine : MonoBehaviour {
 			} else {
 				needsToLerp = true;
 				attached = AttachPoint.Hand;
+				this.rigidbody.isKinematic = true;
 			}
 		}
 
-		//lerp to position
-		//if (needsToLerp) {
 		Transform snapTransform = null;
-		GameObject hand = null;
-		GameObject pistol = null;
 		if (attached == AttachPoint.Hand) {
-			hand = GameObject.FindGameObjectWithTag ("LeftHand");
 			snapTransform = hand.transform;
 		} else if (attached == AttachPoint.Gun) {
-			pistol = GameObject.FindGameObjectWithTag ("Pistol");
 			snapTransform = pistol.transform;
 		}
-		
+
+		//lerp to position
+		if (needsToLerp) {
+			float step = speed * Time.deltaTime;
+			lerpToObject (snapTransform, step);
+		}
+
+	}
+
+	private void lerpToObject(Transform transformMoveTo, float step){
+		Vector3 positionCorrection = Vector3.zero;
+		if (attached == AttachPoint.Gun) {
+			positionCorrection = lerpPistolPosition;
+		} else if (attached == AttachPoint.Hand) {
+			positionCorrection = lerpHandPosition;
+		}
+		transform.position = Vector3.MoveTowards (this.transform.position, new Vector3(transformMoveTo.position.x + positionCorrection.x,
+		                                                                               transformMoveTo.position.y + positionCorrection.y,
+		                                                                               transformMoveTo.position.z + positionCorrection.z), step);
+		//object has now moved to its destination, snap so that it is now child of parent
+		if (this.transform.position == new Vector3 (transformMoveTo.position.x + positionCorrection.x,
+                     transformMoveTo.position.y + positionCorrection.y,
+                     transformMoveTo.position.z + positionCorrection.z)) {
+			needsToLerp = false;
+			snapToObject(transformMoveTo);
+		}
+	}
+
+	private void snapToObject(Transform snapTransform){
+
 		if (snapTransform != null) {
 			Vector3 snapPosition = snapTransform.position;
 			Vector3 positionCorrection = Vector3.zero;
-			if(attached == AttachPoint.Gun){
+			if (attached == AttachPoint.Gun) {
 				positionCorrection = pistolPosition;
 			} else if (attached == AttachPoint.Hand) {
 				positionCorrection = handPosition;
 			}
 			this.transform.position = new Vector3 (snapPosition.x, 
-		                                       snapPosition.y , snapPosition.z);
-			
+                                       snapPosition.y, snapPosition.z);
+
 			this.rigidbody.isKinematic = true;
 			Vector3 rotationVec = Vector3.zero;
-			if(attached == AttachPoint.Hand){
+			if (attached == AttachPoint.Hand) {
 				rotationVec = handRotation;
 				this.transform.parent = hand.transform;
-			}else if (attached == AttachPoint.Gun) {
+			} else if (attached == AttachPoint.Gun) {
 				rotationVec = pistolRotation;
 				this.transform.parent = pistol.transform;
 			}
 			this.transform.localPosition = Vector3.zero; 
 
-			if(rotationVec != Vector3.zero){
-				this.transform.localRotation = Quaternion.Euler(rotationVec.x, rotationVec.y, rotationVec.z);
+			if (rotationVec != Vector3.zero) {
+				this.transform.localRotation = Quaternion.Euler (rotationVec.x, rotationVec.y, rotationVec.z);
 			}
-			if(positionCorrection != Vector3.zero){
+			if (positionCorrection != Vector3.zero) {
 				this.transform.localPosition = positionCorrection;
 			}
-		}
-		//}
-
-	}
-
-	private void lerpToPosition (Vector3 startPos, Vector3 endPos, float time) {
-		float i = 0.0f;
-		float rate = 1.0f/time;
-		while (i < 1.0f) {
-			i += Time.deltaTime * rate;
-			this.transform.position = Vector3.Lerp(startPos, endPos, i);
 		}
 	}
 
