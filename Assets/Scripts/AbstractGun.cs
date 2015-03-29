10ce2b87052592e73e955fa1e66299e04a06bea1
Waylon Brown <waylonjbrown@gmail.com>
2015-03-29 14:36:06 -0500

    Weapons slide receives feedback from shooting

--- Assets/Scripts/AbstractGun.cs
+++ Assets/Scripts/AbstractGun.cs
@@ -4,36 +4,29 @@ using System.Collections;
 public abstract class AbstractGun : Pickupable {
 	public Vector3 barrel_offset;
 	GameObject player;
+	GunSlide gunSlide;
 	Pickup_Object playerFunctions;
 	public Transform bulletHole;
 
 	// Use this for initialization
 	protected void Start () {
 		player = GameObject.FindWithTag("Player");
-
-		//Debug.Log(player.transform.position);
-
+		gunSlide = (GunSlide)GameObject.FindWithTag ("GunSlide").GetComponent(typeof(GunSlide));
 		playerFunctions = player.GetComponent<Pickup_Object> ();
 	}
 
 	public override void carryUpdate(Hand hand)
 	{
 
-
+		//if gun is shot
 		if(((hand.rightHand && Input.GetMouseButtonDown(0)) || (!hand.rightHand && Input.GetMouseButtonDown(1))))
 		{
 				//Debug.Log(playerFunctions.carriedObject.transform.TransformDirection(Vector3.forward));
-
+			gunSlide.slideAction();
 			GetComponent<AudioSource>().Play ();
-
 			Vector3 fwd = /*Quaternion.Euler (this.handRotationOffset) * */ this.transform.forward;
-
-			Debug.Log (fwd);
-
 			Ray gunDirection = new Ray(this.gameObject.transform.position /*+ this.gameObject.transform.TransformDirection(barrel_offset)*/, fwd * 50);
-
 			LayerMask layerMask = 1 << 1; //This ignores the SafetyMonitor, meaning the raycast will ignore the range plane when shooting
-
 			RaycastHit hit;
 
 			if(Physics.Raycast (gunDirection, out hit, layerMask))
